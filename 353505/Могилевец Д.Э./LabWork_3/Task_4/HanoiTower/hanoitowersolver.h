755289b266fddf9374604502ca8349cb4c82c9fe
Selleihtr <160356638+Selleihtr@users.noreply.github.com>
2024-05-02 15:02:34 +0300

    add labworks 2,3,4

--- 353505/Могилевец Д.Э./LabWork_3/Task_4/HanoiTower/hanoitowersolver.h
+++ 353505/Могилевец Д.Э./LabWork_3/Task_4/HanoiTower/hanoitowersolver.h
@@ -1,13 +1,1 @@
-#ifndef HANOITOWERSOLVER_H
-#define HANOITOWERSOLVER_H
-
-#include <QTableWidget>
-
-class HanoiTowerSolver
-{
-public:
-    HanoiTowerSolver();
-    void Solve(QTableWidget*, int, int, int&, int);
-};
-
-#endif // HANOITOWERSOLVER_H
+	
