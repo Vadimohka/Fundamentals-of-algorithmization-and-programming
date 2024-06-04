63003ae878b4f7469ec5e88ac03b22167f9f833c
HECMOTRI <stanrannyshot@gmail.com>
2024-06-05 00:36:37 +0300

    Курсовая работа Жукова Артема 353502

---
+++ 353502/Жуков А.Д./Курсовая работа/server/databasemanager.h
@@ -0,0 +1,42 @@
+#ifndef DATABASEMANAGER_H
+#define DATABASEMANAGER_H
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSql>
+#include <QSqlError>
+#include <ctime>
+
+#include "DatabaseQuerys.h"
+
+class DatabaseManager
+{
+public:
+    DatabaseManager();
+
+    bool registerUser(const QString &, const QString &password);
+    bool userExist(const QString &, const QString &password = "");
+    bool createChat(const QString &chatName, bool isGroup, const QList<int> &memberIds);
+    void addMessage(const QString &message, const QString &fromUser, const QString &toUser, const QString &isGroup, const QString &type, const QString &file = "");
+    QStringList loadMessages(const QString &author, const QString &to);
+    QString &LoadFile(const QString &author, const QString &to);
+    QStringList &getMemebersIds(const QString &chatName);
+
+    QString timeNow();
+    QString dateNow();
+
+    QString getUID(const QString & login);
+    QString getUsersByKey(const QString & key, const QString & exceptId);
+    QString getUserChatNames(int userId);
+    QString getNameById(const QString &id);
+    int getChatId(const QString &chatName, const QString &fromUser, bool &isGroup);
+    //QStringList* getMessages(QString forUser, QString fromUser);
+
+private:
+    QSqlDatabase database_users;
+    QSqlQuery* query;
+
+    bool pickChat(const QString &, const QString &);
+};
+
+#endif // DATABASEMANAGER_H
