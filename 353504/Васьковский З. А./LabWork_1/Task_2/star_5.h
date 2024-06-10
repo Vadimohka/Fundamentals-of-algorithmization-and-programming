9f5ae438b4360e6250207a848dcc1ecab9ac8a13
panigirik <160933472+panigirik@users.noreply.github.com>
2024-06-10 16:29:12 +0300

    Add files via upload
    Added labs: 1, 2, 5, 8, added coursework

---
+++ 353504/Васьковский З. А./LabWork_1/Task_2/star_5.h
@@ -0,0 +1,13 @@
+
+#ifndef STAR_5_H
+#define STAR_5_H
+
+#include "star.h"
+
+class Star_5 : public Star
+{
+public:
+    Star_5();
+};
+
+#endif // STAR_5_H
---
+++ 353504/Васьковский З. А./LabWork_2/Task_2/viev_dialog.cpp
@@ -0,0 +1,3 @@
+#include "viev_dialog.h"
+
+viev_dialog::viev_dialog() {}
